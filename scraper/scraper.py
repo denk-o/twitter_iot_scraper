72af42d7892d4b3c1fa17be69de2b259c076302c
Kevin Liu <kevinliu314@gmail.com>
2018-07-24 23:05:39 -0500

    initial file setup

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,1 +1,18 @@
 #twitter twitter_iot_scraper
+import concurrent.futures
+import time
+import os
+
+class scraper():
+    def __init__(self):
+        self.buildDir()
+
+    def buildDir(self):
+        if os.path.isdir('csv_results/'):
+            os.mkdir('csv_results/')
+
+    def getTweetsWithHashtag(self, hashtag):
+        #get tweets with a hashtag specified as input
+
+    def writeTweetsToCSV(self):
+        #write tweets to a csv
